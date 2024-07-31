33165fb7b01923fa6e17164eef68596f25f339df
alexandreribeiro7 <alexanddreesantos@gmail.com>
2024-07-31 12:39:59 -0300

    feature/add views para listar e exibir agendamento

--- agenda/models.py
+++ agenda/models.py
@@ -2,4 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Agendamento(models.Model):
-    data_horario
+    data_horario = models.DateTimeField()
+    name_cliente = models.CharField(max_length=200)
+    email_cliente = models.EmailField()
+    telefone_cliente = models.CharField(max_length=20)
