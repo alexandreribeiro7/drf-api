d0da85ed7cd479888c94b03702e98869c53f860e
alexandreribeiro7 <alexanddreesantos@gmail.com>
2024-08-07 18:12:43 -0300

    add serializers

--- agenda/serializers.py
+++ agenda/serializers.py
@@ -1,8 +1,26 @@
+import datetime
 from rest_framework import serializers
+from django.utils import timezone
+
+from agenda.models import Agendamento
 
 
-class AgendamentoSerializer(serializers.Serializer):
-    data_horario = serializers.DateTimeField()
-    name_cliente = serializers.CharField(max_length=200)
-    email_cliente = serializers.EmailField()
-    telefone_cliente = serializers.CharField(max_length=20)
+class AgendamentoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Agendamento
+        fields = ['id', 'data_horario', 'name_cliente', 'email_cliente', 'telefone_cliente']
+    
+    def validate_data_horario(self, value):
+        if value < timezone.now():
+            raise serializers.ValidationError("Agendamento não pode ser feito no passado!")
+        return value
+    
+    def validate(self, attrs):
+        telefone_cliente = attrs.get("telefone_cliente")
+        email_cliente = attrs.get("email_cliente")
+        
+        if email_cliente and telefone_cliente:
+            if email_cliente.endswith(".br") and telefone_cliente.startswith("+") and not telefone_cliente.startswith("+55"):
+                raise serializers.ValidationError("E-mail brasileiro deve estar associado a um número do Brasil (+55)")
+
+        return attrs
--- agenda/views.py
+++ agenda/views.py
@@ -1,3 +1,4 @@
+from urllib import response
 from django.http import JsonResponse
 from django.shortcuts import get_object_or_404
 from rest_framework.decorators import api_view
@@ -6,24 +7,22 @@ from agenda.models import Agendamento
 from agenda.serializers import AgendamentoSerializer
 
 # Create your views here.
-@api_view(http_method_names=["GET", "PUT"])
+@api_view(http_method_names=["GET", "PATCH", "DELETE"])
 def agendamento_detail(request, id):
+    obj = get_object_or_404(Agendamento, id=id)
     if request.method == "GET":
-        obj = get_object_or_404(Agendamento, id=id)
         serializer = AgendamentoSerializer(obj)
         return JsonResponse(serializer.data)
-    if request.method == "PUT":
-        obj = get_object_or_404(Agendamento, id=id)
-        serializer = AgendamentoSerializer(data=request.data)
+    if request.method == "PATCH":
+        serializer = AgendamentoSerializer(obj, data=request.data, partial=True)
         if serializer.is_valid():
-            validated_data = serializer.validated_data
-            obj.data_horario = validated_data.get("data_horario", obj.data_horario)
-            obj.name_cliente = validated_data.get("name_cliente", obj.name_cliente)
-            obj.email_cliente = validated_data.get("email_cliente", obj.email_cliente)
-            obj.telefone_cliente = validated_data.get("telefone_cliente", obj.telefone_cliente)
-            obj.save()
+            serializer.save()
             return JsonResponse(serializer.data, status=200)
         return JsonResponse(serializer.errors, status=400)
+    if request.method == "DELETE":
+        obj.delete()
+        obj.cancelado = True
+        return response(status=204)
 
 @api_view(http_method_names=["GET", "POST"])
 def agendamento_list(request):
@@ -35,12 +34,6 @@ def agendamento_list(request):
         data = request.data
         serializer = AgendamentoSerializer(data=data)
         if serializer.is_valid():
-            validated_data = serializer.validated_data
-            Agendamento.objects.create(
-                data_horario=validated_data["data_horario"],
-                name_cliente=validated_data["name_cliente"],
-                email_cliente=validated_data["email_cliente"],
-                telefone_cliente=validated_data["telefone_cliente"],
-            )
+            serializer.save()
             return JsonResponse(serializer.data, status=200)
         return JsonResponse(serializer.errors, status=400)
