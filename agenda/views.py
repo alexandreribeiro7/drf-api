7227ffdff73449d60147df8e6591b8a4035fff66
alexandreribeiro7 <alexanddreesantos@gmail.com>
2025-07-29 20:23:27 -0300

    Adiciona endpoints de listagem e detalhe para Agendamento
    - add urls.py
    - add models
    - add serializers
    - add views

--- agenda/views.py
+++ agenda/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from django.shortcuts import get_object_or_404
+from rest_framework.decorators import api_view
 
-# Create your views here.
+from agenda.models import Agendamento
+from agenda.serializers import AgendamentoSerializer
+
+@api_view(http_method_names=['GET'])
+def agendamento_detail(request, id):
+    obj = get_object_or_404(Agendamento, id=id)
+    serializer = AgendamentoSerializer(obj)
+    return JsonResponse(serializer.data)
+
+@api_view(http_method_names=['GET'])
+def agendamento_list(request):
+    queryset = Agendamento.objects.all()
+    serializer = AgendamentoSerializer(queryset, many=True)
+    return JsonResponse(serializer.data, safe=False)
