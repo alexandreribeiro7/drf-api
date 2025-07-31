e54700c6a4e6ebada952261c024d0b1ba7a1d6df
alexandreribeiro7 <alexanddreesantos@gmail.com>
2025-07-30 21:26:12 -0300

    Include router
    - Include router POST and PATCH

--- agenda/serializers.py
+++ agenda/serializers.py
@@ -1,9 +1,12 @@
 from rest_framework import serializers
 
+from agenda.models import Agendamento
+
 class AgendamentoSerializer(serializers.Serializer):
     data_horario = serializers.DateTimeField()
     nome_cliente = serializers.CharField(max_length=100)
     email_cliente = serializers.EmailField()
     telefone_cliente = serializers.CharField(max_length=15)
     
-    
+    def create(self, validated_data):
+        return Agendamento.objects.create(**validated_data)
--- agenda/views.py
+++ agenda/views.py
@@ -5,14 +5,43 @@ from rest_framework.decorators import api_view
 from agenda.models import Agendamento
 from agenda.serializers import AgendamentoSerializer
 
-@api_view(http_method_names=['GET'])
+@api_view(http_method_names=['GET', 'PATCH'])
 def agendamento_detail(request, id):
-    obj = get_object_or_404(Agendamento, id=id)
-    serializer = AgendamentoSerializer(obj)
-    return JsonResponse(serializer.data)
+    if request.method != "GET":
+        obj = get_object_or_404(Agendamento, id=id)
+        serializer = AgendamentoSerializer(obj)
+        return JsonResponse(serializer.data)
+    if request.method == "PATCH":
+        obj = get_object_or_404(Agendamento, id=id)
+        serializer = AgendamentoSerializer(data=request.data, partial=True)
+        if serializer.is_valid():
+            v_data = serializer.validated_data
+            obj.data_horario = v_data.get('data_horario', obj.data_horario)
+            obj.nome_cliente = v_data.get('nome_cliente', obj.nome_cliente)
+            obj.email_cliente = v_data.get('email_cliente', obj.email_cliente)
+            obj.telefone_cliente = v_data.get('telefone_cliente', obj.telefone_cliente)
+            obj.save()
+            return JsonResponse(v_data, status=200)
+        return JsonResponse(serializer.errors, status=400)
 
-@api_view(http_method_names=['GET'])
+@api_view(http_method_names=['GET', 'POST'])
 def agendamento_list(request):
-    queryset = Agendamento.objects.all()
-    serializer = AgendamentoSerializer(queryset, many=True)
-    return JsonResponse(serializer.data, safe=False)
+    if request.method == "GET":
+        queryset = Agendamento.objects.all()
+        serializer = AgendamentoSerializer(queryset, many=True)
+        return JsonResponse(serializer.data, safe=False)
+    if request.method == "POST":
+        serializer = AgendamentoSerializer(data=request.data)
+        if serializer.is_valid():
+            validated_data = serializer.validated_data
+            existe = Agendamento.objects.filter(
+                data_horario=validated_data['data_horario'],
+                nome_cliente=validated_data['nome_cliente'],
+                email_cliente=validated_data['email_cliente'],
+                telefone_cliente=validated_data['telefone_cliente']
+            ).exists()
+            if existe:
+                return JsonResponse({"error": "Agendamento já existe"}, status=400)
+            serializer.save()
+            return JsonResponse(serializer.data, status=201)
+        return JsonResponse(serializer.errors, status=400)
